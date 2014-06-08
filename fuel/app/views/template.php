6fbbab469fd97af89aff7a34e0ac8434ad2b611e
Conor Smith <conorjbsmith@gmail.com>
2014-06-08 18:41:58 +0000

    Optimised the application for mobile
    I added a new logo for the application to the project. I created a favicon and mobile homescreen icon which have been added to the base template. I also added meta tags to allow the application to be added to mobile homescreens.

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -1,6 +1,14 @@
 <html lang="en">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes">
+
+<link rel="shortcut icon" href="/assets/img/favicon.ico">
+<link rel="icon" sizes="196x196" href="/assets/img/app-icon-196.png">
+<link rel="icon" sizes="128x128" href="/assets/img/app-icon-128.png">
+<link rel="apple-touch-icon" sizes="128x128" href="/assets/img/app-icon-128.png">
+
 <link type="text/css" rel="stylesheet" href="/assets/css/bootstrap.css">
 <link type="text/css" rel="stylesheet" href="/assets/css/bootstrap-responsive.css">
 <link type="text/css" rel="stylesheet" href="/assets/css/style.css">
